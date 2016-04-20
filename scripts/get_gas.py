125dc443bdecb52d54f1519f2c5264ac9f6e1c82
Dan Nguyen <dansonguyen@gmail.com>
2016-04-20 14:55:29 -0700

    afasdfasdf

--- scripts/get_gas.py
+++ scripts/get_gas.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+
+
 fname = 'mydata/crime-alert1415.shtml'
 
 
@@ -6,18 +8,24 @@ txt = open(fname).read()
 soup = BeautifulSoup(txt, 'lxml')
 
 events_list = []
-for hedtag in soup.find_all('h3'):
-    title = hedtag.text
-    if 'gas' in title.lower():
-        eventtxt = title + "\n"
-        for ptag in hedtag.find_next_siblings('p'):
-            eventtxt += ptag.text
-            eventtxt += "\n"
-            pid = ptag.attrs.get('id')
-            if pid == 'post-date':
-                break
 
-        events_list.append(eventtxt)
+
+for hedtag in soup.find_all('h3'):
+    titletxt = hedtag.text
+    if 'gas' in titletxt.lower():
+        mydict = {}
+        mydict['title'] = titletxt
+        mydict['body'] = ""
+
+        for ptag in hedtag.find_next_siblings('p'):
+            pd = ptag.attrs.get('id')
+            if pd == 'post-date':
+                mydict['post-date'] = ptag.text
+                break
+            else:
+                mydict['body'] += ptag.text + "\n"
+
+        events_list.append(mydict)
 
 
 for e in events_list:
